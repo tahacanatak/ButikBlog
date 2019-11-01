aa8ec5386086e5fc76818f12bb9161a6a88bc562
tahacan.atak <tahacan.atak@gmail.com>
2019-11-01 18:13:54 +0300

    Add project files.

---
+++ ButikBlog/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ButikBlog.MvcApplication" Language="C#" %>
---
+++ ButikBlog/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ButikBlog.Startup))]
+namespace ButikBlog
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
