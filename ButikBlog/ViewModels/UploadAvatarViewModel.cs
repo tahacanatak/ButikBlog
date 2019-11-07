52ae73b374ecf25ea76ede8a4e26486f02693b62
tahacan.atak <tahacan.atak@gmail.com>
2019-11-07 21:32:23 +0300

    Add UploadPhoto Attribute

--- ButikBlog/ViewModels/UploadAvatarViewModel.cs
+++ ButikBlog/ViewModels/UploadAvatarViewModel.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using ButikBlog.Attributes;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -9,6 +11,8 @@ namespace ButikBlog.ViewModels
     {
         public string Photo { get; set; }
 
+        [Required(ErrorMessage = "Resim dosyası seçmediniz.")]
+        [ProfilePhoto(MaxFileSize = 1000000, ErrorMessage = "1 Mb'den küçük bir dosya giriniz.")]
         public HttpPostedFileBase File { get; set; }
     }
 }
